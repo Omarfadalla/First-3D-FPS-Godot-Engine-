6885a0e3f93a07741a85ef10b3164ccdaa3e789a
Omarfadalla <omarfadalla@gmail.com>
2026-06-14 17:49:09 +0300

    Add files via upload
    added a new scene and add zombie and player movement mechanics along with floors and obstacles !
    next part will add a gun to player and improve zombie script

---
+++ 3d-fps/Scenes/zombie.gd
@@ -0,0 +1,30 @@
+extends CharacterBody3D
+
+
+@export var speed = 5
+@export var gravity = - 10
+@onready var navigation_agent_3d: NavigationAgent3D = $NavigationAgent3D
+@onready var player: CharacterBody3D = %Player 
+
+
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity
+	
+	var dir = to_local(navigation_agent_3d.get_next_path_position()).normalized()
+	$holder.look_at(player.position)
+	velocity = dir * speed
+
+	move_and_slide()
+	pass
+	
+func _ready() -> void:
+	$holder/AnimationPlayer.play("walking")
+	pass
+
+func make_path():
+	navigation_agent_3d.target_position = player.global_position
+	pass
+func _on_timer_timeout() -> void:
+	make_path() 
+	pass # Replace with function body.
